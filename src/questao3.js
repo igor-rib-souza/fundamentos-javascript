530ba6de42d5c1a88a48e10b27d6420b52a43e43
Igor Souza <igorrs.rib@gmail.com>
2023-06-24 02:47:33 -0300

    feat: add user prompts

--- src/questao3.js
+++ src/questao3.js
@@ -24,13 +24,12 @@ class Poligono {
     perimetro() {
         let perimetro = 0;
 
-        for (let i = 0; i < this.#vertices.length - 1; i++) {
+        for (let i = 0; i < this.#vertices.length - 2; i++) {
             const verticeAtual = this.#vertices[i];
             const proximoVertice = this.#vertices[i + 1];
             perimetro += verticeAtual.distancia(proximoVertice);
         }
-
-        perimetro += this.#vertices[this.#vertices.length - 1].distancia(this.#vertices[0]);
+        perimetro += this.#vertices[this.#vertices.length - 2].distancia(this.#vertices[0]);
 
         return perimetro;
     }
@@ -41,3 +40,32 @@ class Poligono {
 }
 
 export default Poligono;
+
+const prompt = PromptSync({ sigint: true });
+
+const x1 = prompt("Digite o x do primeiro vértice ");
+const y1 = prompt("Digite o y do primeiro vértice");
+const vertice1 = new Vertice(x1, y1)
+
+const x2 = prompt("Digite o x do segundo vértice ");
+const y2 = prompt("Digite o y do segundo vértice ");
+const vertice2 = new Vertice(x2, y2);
+
+const x3 = prompt("Digite o x do terceiro vértice ");
+const y3 = prompt("Digite o y do terceio vértice ");
+const vertice3 = new Vertice(x3, y3);
+
+const edges = [vertice1, vertice2, vertice3]
+
+const polígono = new Poligono(edges);
+
+//addvertice 
+const x4 = prompt("Digite o x do quarto vértice ");
+const y4 = prompt("Digite o y do quarto vértice ");
+console.log(polígono.addVertice(x4,y4));
+
+//perimetro
+console.log(polígono.perimetro());
+
+//qtdvertices
+console.log(polígono.qtdVertices());
