8745f216765ce01e772be5167692202e6298cd64
Igor Souza <igorrs.rib@gmail.com>
2023-06-23 22:02:37 -0300

    feat: add function tests

--- src/questao1.js
+++ src/questao1.js
@@ -5,8 +5,8 @@ class Vertice{
     #y;
 
     constructor(x,y){
-        this.x = x;
-        this.y = y;
+        this.#x = x;
+        this.#y = y;
     }
 
     // leitura pública do atributo x
@@ -20,10 +20,10 @@ class Vertice{
     }
 
     distancia(otherVertice){
-        outroX = otherVertice.x;
-        outroY = otherVertice.y;
+        const outroX = otherVertice.#x;
+        const outroY = otherVertice.#y;
 
-        distance = (((outroX - this.#x)**2) + ((outroY - this.#y)**2))**0.5;
+        return (((outroX - this.#x)**2) + ((outroY - this.#y)**2))**0.5;
     }
 
     move(x,y){
@@ -37,4 +37,46 @@ class Vertice{
     }
 }
 
-export default Vertice;
+
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const x1 = prompt("Digite o x do primeiro vértice ");
+const y1 = prompt("Digite o y do primeiro vértice");
+const vertice1 = new Vertice(x1,y1)
+
+const x2 = prompt("Digite o x do segundo vértice ");
+const y2 = prompt("Digite o y do segundo vértice ");
+const vertice2 = new Vertice(x2,y2);
+
+const x3 = prompt("Digite o x do terceiro vértice ");
+const y3 = prompt("Digite o y do terceio vértice ");
+const vertice3 = new Vertice(x3,y3);
+
+// getter x
+console.log(vertice1.x);
+console.log(vertice2.x);
+console.log(vertice3.x);
+
+// getter y
+console.log(vertice1.y);
+console.log(vertice2.y);
+console.log(vertice3.y);
+
+// distancia
+console.log(vertice1.distancia(vertice2));
+console.log(vertice1.distancia(vertice3));
+console.log(vertice2.distancia(vertice3));
+
+// move
+console.log(vertice1.x);
+console.log(vertice1.y);
+console.log("Os valores acima são antes do move, os abaixo são após");
+vertice1.move(vertice2.x,vertice2.y);
+console.log(vertice1.x);
+console.log(vertice1.y);
+
+// equals
+console.log(vertice1.equals(vertice2));
+console.log(vertice1.equals(vertice3));
+console.log(vertice2.equals(vertice3))
